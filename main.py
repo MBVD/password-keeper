d9b1454144a69655efade88c80bd841dbdb8bb1e
MBVD <vasiliidiu3102@gmail.com>
2022-08-29 21:48:09 +0600

    some new features and back start

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+from xml.dom.minidom import Element
 import eel
+import  pyperclip
+import pandas as pd 
+import numpy as np
+def copy_element():
+    element = get_element()
+    pyperclip.copy(element)
 
-eel.init("web")
-eel.start("index.html", size = (700, 700))
+def get_element (name):
+    df = pd.read_csv("base.csv")
+    return df.loc[df["name"] == name]["key"]
+
+def save_element(name, key):
+    d_frame = pd.read_csv('base.csv')
+    print(d_frame)
+    d_frame.loc[len(d_frame.index)] = [name, key]
+    d_frame.to_csv("base.csv")
+    
+
+# eel.init("web")
+# eel.start("index.html", size = (700, 700))
