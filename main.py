4fa3cc96753ef9bb1deb2030c1f952d5530cc80f
MBVD <vasiliidiu3102@gmail.com>
2022-08-30 21:13:59 +0600

    working save and get_element funct

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from webbrowser import get
 from xml.dom.minidom import Element
 import eel
 import  pyperclip
@@ -8,14 +9,19 @@ def copy_element():
     pyperclip.copy(element)
 
 def get_element (name):
-    df = pd.read_csv("base.csv")
-    return df.loc[df["name"] == name]["key"]
+    try:
+        df = pd.read_csv("base.csv")
+        return df.loc[df["name"] == name]["key"].to_dict()[0]
+    except Exception as e:
+        return None
 
 def save_element(name, key):
     d_frame = pd.read_csv('base.csv')
-    print(d_frame)
-    d_frame.loc[len(d_frame.index)] = [name, key]
-    d_frame.to_csv("base.csv")
+    if get_element(name):
+        return False
+    d_frame.loc[len(d_frame), d_frame.columns] = name, key
+    d_frame.to_csv("base.csv",index=False)
+    return True
     
 
 # eel.init("web")
