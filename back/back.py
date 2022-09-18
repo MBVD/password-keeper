a24776fe69ee6ba3ed069a05f5f02409363c8437
MBVD <vasiliidiu3102@gmail.com>
2022-09-18 19:39:49 +0600

    delete service password

--- back/back.py
+++ back/back.py
@@ -5,8 +5,9 @@ import json
 from logutils import logger
 import config as cfg
 
+
 @eel.expose
-def get_element (name):
+def get_element(name):
     try:
         df = pd.read_csv(cfg.CSV_FILE)
         element_dict = df.loc[df["name"] == name.replace(' ', '')]["key"].to_dict()
@@ -14,21 +15,37 @@ def get_element (name):
         key = list(element_dict.keys())[0]
         element = element_dict[key]
         pyperclip.copy(element)
-        logger.info(element)
-        return element
+        logger.info("Пароль " + element + " индекс - " + str(key))
+        return element, key
     except Exception as e:
         if type(e) is IndexError:
             logger.info(None)
-            return None
+            return None, None
         logger.info(str(e))
-        return None
+        return None, None
+
 
 @eel.expose
 def save_element(name, key):
-    d_frame = pd.read_csv(cfg.CSV_FILE)   
-    if get_element(name):
+    d_frame = pd.read_csv(cfg.CSV_FILE)
+    element, key1 = get_element(name)
+    if element:
         return False
     d_frame.loc[len(d_frame), d_frame.columns] = name, key, 0
-    d_frame.to_csv(cfg.CSV_FILE,index=False)
-    logger.info("Успешно сохранили Сервис: {name}".format(name = name))
+    d_frame.to_csv(cfg.CSV_FILE, index=False)
+    logger.info("Успешно сохранили Сервис: {name}".format(name=name))
     return True
+
+
+@eel.expose
+def delete_element(name):
+    d_frame = pd.read_csv(cfg.CSV_FILE)
+    element, key = get_element(name)
+    if key is None:
+        logger.info("Невозможно удалить элемент {name}".format(name=name))
+        return False
+    else:
+        d_frame = d_frame.drop([key])
+        logger.info("Удален Элемент - {service} с паролем - {element}".format(element=element, service=name))
+        d_frame.to_csv(cfg.CSV_FILE, index=False)
+        return True
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import eel
 from back.back import *
 
 eel.init("web")
-eel.start("index.html", size = (700, 700))
+eel.start("index.html", size=(700, 700))
